e43ff85e924cfa8a078d53859ce53ed79108bc25
oxtna <kajomof.rafal@gmail.com>
2024-10-22 01:37:16 +0200

    Remove windows specific config from Neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,17 +14,6 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '>·', trail = '·', nbsp = '·' }
 vim.opt.shortmess = vim.opt.shortmess + 'sS'
 
--- Use Powershell on Windows
-if vim.loop.os_uname().sysname == 'Windows_NT' then
-  vim.opt.shell = vim.fn.executable('pwsh') and 'pwsh' or 'powershell'
-  vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command \z
-    [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-  vim.opt.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
-  vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-  vim.opt.shellquote = ''
-  vim.opt.shellxquote = ''
-end
-
 -- Better line wrapping
 vim.opt.showbreak = ' ¬'
 vim.opt.breakindent = true
